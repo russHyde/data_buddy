c22bc0d573f0dadf5f72741f4c23150e1dfa9f72
Russell Hyde <russ_hyde@hotmail.com>
2019-01-04 12:41:04 +0000

    use black for styling & add pylint config

--- {{cookiecutter.project_name}}/scripts/helpers_for_setup/buddy/buddy/setup_git_clones.py
+++ {{cookiecutter.project_name}}/scripts/helpers_for_setup/buddy/buddy/setup_git_clones.py
@@ -1,31 +1,49 @@
+"""
+Functions for identifying which git repos need to be cloned for the current
+project, and for cloning them
+"""
+
 import yaml
 
 from buddy.git_classes import ExternalRepository
 
 
 def read_repository_details(yaml_file):
-    yaml_dict = yaml.load(open(yaml_file, 'r'))
+    """
+    Reads all data stored in a yaml file; returns a dictionary storing the
+    key-value pairs within the file
+    """
+    yaml_dict = yaml.load(open(yaml_file, "r"))
     if yaml_dict is None:
         return {}
-    else:
-        return yaml_dict
+    return yaml_dict
 
 
 def parse_repository_details(yaml_dictionary):
+    """
+    Extracts details of git repositories: where are they stored, where are they
+    to be copied, which commit should be checked out?
+    """
     repositories = {
-        k: ExternalRepository(v['url'], v['commit'], v['output'])
+        k: ExternalRepository(v["url"], v["commit"], v["output"])
         for k, v in yaml_dictionary.items()
     }
     return repositories
 
 
-def import_repository_details(yaml):
-    # read_repository_details()
-    # parse_repository_details()
+def import_repository_details(yaml_file):
+    """
+    Reads and extracts repository information from a yaml file
+    """
+    # yaml_dict = read_repository_details(yaml_file)
+    # return parse_repository_details(yaml_dict)
     return {}
 
 
 def get_command_args():
+    """
+    Extract command args that were used when calling this program
+    """
     return {}
 
 
