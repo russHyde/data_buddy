509d3394cdfccc326e0c26d6a56390f708cbafab
Russ Hyde <russHyde@users.noreply.github.com>
2019-01-21 11:57:47 +0000

    export R_INCLUDES_FILE if an R package is required
    Also updates some comments

--- {{cookiecutter.project_name}}/.setup_config/job_specific_vars.sh
+++ {{cookiecutter.project_name}}/.setup_config/job_specific_vars.sh
@@ -1,35 +1,37 @@
 #!/bin/bash -e
 
 ###############################################################################
-# 24/1/2017
-# User-specified variables for use in the drug_markers project
+# User-specified variables for use in the `{{cookiecutter.project_name}}`
+# project
 
 ###############################################################################
-# JOBNAME and IS_R_REQUIRED must be defined for each new project
+# `JOBNAME` and `IS_R_REQUIRED` must be defined for each new project
 #
-# IS_R_REQUIRED=1 is required if an R kernel is to be made or R is to be used
-# IS_R_PKG_REQUIRED=1 is additionally required if an R package is to be made
+# `IS_R_REQUIRED=1` if R is to be used in addition to pythono within the current
+# project
+# `IS_R_PKG_REQUIRED=1` is required if an R package is to be made
+# `IS_JUPYTER_R_REQUIRED=1` is required if an R kernel is to be made
+
 export JOBNAME="{{cookiecutter.project_name}}"
 export IS_R_REQUIRED={{cookiecutter.is_r_required}}
 export IS_R_PKG_REQUIRED={{cookiecutter.is_r_pkg_required}}
 export IS_JUPYTER_R_REQUIRED={{cookiecutter.is_jupyter_r_kernel_required}}
 
-# Nonessential variables:
-#
-# - Define which external R packages are 'include'd by the job-specific package
-export R_INCLUDES_FILE="${PWD}/lib/conf/include_into_rpackage.txt"
-
 ###############################################################################
-# PKGNAME and ENVNAME can be overridden by the user
+# PKGNAME is the name of the job-specific R package
 # - Note that the R-package <PKGNAME> will only be built/installed if the user
 #   adds some files to the subdirectories ./lib/local_rfuncs/R or
 #   ./lib/global_rfuncs
-#
+# - The R_INCLUDES_FILE defines which external R packages are 'include'd by the
+#   job-specific package
+
 if [[ ! -z "${IS_R_PKG_REQUIRED}" ]] && [[ ${IS_R_PKG_REQUIRED} -ne 0 ]];
 then
   export PKGNAME="{{cookiecutter.r_pkg_name}}"
+  export R_INCLUDES_FILE="${PWD}/lib/conf/include_into_rpackage.txt"
 fi
 
+# ENVNAME is the name of the job-specific conda environment
 export ENVNAME="{{cookiecutter.conda_env}}"
 
 ###############################################################################
