6e48addf0df6a274c14c99bfc0a39e7e4a57faa4
Russell Hyde <russ_hyde@hotmail.com>
2019-02-06 15:00:40 +0000

    rename function: parse_validator_details

--- {{cookiecutter.project_name}}/scripts/helpers_for_setup/buddy/buddy/validate_file_contents.py
+++ {{cookiecutter.project_name}}/scripts/helpers_for_setup/buddy/buddy/validate_file_contents.py
@@ -2,6 +2,7 @@ import argparse
 import sh
 import sys
 
+
 def get_md5sum(filepath):
     try:
         md5 = str(sh.md5sum(filepath)).strip().split()[0]
@@ -11,14 +12,13 @@ def get_md5sum(filepath):
         sys.exit(1)
 
 
-def parse_validation_test_details(yaml_dictionary):
+def parse_validator_details(yaml_dictionary):
     # each test is of the form {test_name: {key1: value1, key2: value2, ...}}
     # - To compare md5sum between a file and a string, one of the keys should be
     # `md5sum` and another should be `input_file`
 
     # For each validation test in the dictionary, return an object with a
     # `validate` method
-
     pass
 
 
--- {{cookiecutter.project_name}}/scripts/helpers_for_setup/buddy/tests/unit_tests/test_validate_file_contents.py
+++ {{cookiecutter.project_name}}/scripts/helpers_for_setup/buddy/tests/unit_tests/test_validate_file_contents.py
@@ -1,7 +1,10 @@
+from buddy.validate_file_contents import parse_validator_details
+
+import pytest
+
 # ----
 
 # user
-# .. can ensure the md5sum for a file matches a given value
 # .. can import validation-test definitions from a .yaml file
 # .. can ensure two files have row-subset-matching values
 # .. can ensure two files have column-subset-matching values
@@ -9,6 +12,22 @@
 
 # program
 # .. ensures that validation-test definitions conform to a schema
+# .. can convert free-text definition of validation-test into Validator objects
 
 # ----
 
+class TestParseValidatorDetails(object):
+
+    @pytest.mark.skip(reason="parse_validator is not implemented")
+    def test_md5sum_validators_can_be_parsed(self):
+        yaml_dict = {
+            "test1": {"input_file": "some_file", "expect_md5sum": "a" * 32},
+            "test2": {"input_file": "another_file", "expect_md5sum": "b" * 32}
+        }
+        validators = parse_validator_details(yaml_dict)
+        assert all(
+            map(
+                lambda x: isinstance(x, Md5sumValidator),
+                validators.values()
+            )
+        )
