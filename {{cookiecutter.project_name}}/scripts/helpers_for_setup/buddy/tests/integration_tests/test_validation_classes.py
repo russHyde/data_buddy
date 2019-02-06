3a52f9914407d072c468ce8d97865c0d014973c0
Russell Hyde <russ_hyde@hotmail.com>
2019-02-06 15:57:29 +0000

    styling

--- {{cookiecutter.project_name}}/scripts/helpers_for_setup/buddy/tests/integration_tests/test_validation_classes.py
+++ {{cookiecutter.project_name}}/scripts/helpers_for_setup/buddy/tests/integration_tests/test_validation_classes.py
@@ -7,8 +7,8 @@ from buddy.validation_classes import get_md5sum
 
 # user can compare the md5sum of a file to a reference
 
-class TestMd5sum(object):
 
+class TestMd5sum(object):
     def test_md5sum_for_existing_files(self, tmpdir):
         with sh.pushd(tmpdir):
             empty_string_md5 = hashlib.md5("".encode("utf-8")).hexdigest()
