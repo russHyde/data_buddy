04eb347b945e05348a1f48d381c9f30c2d5106a9
Russell Hyde <russ_hyde@hotmail.com>
2019-05-09 18:04:13 +0100

    isolate jupyter-kernel setup

--- {{cookiecutter.project_name}}/scripts/setup.sh
+++ {{cookiecutter.project_name}}/scripts/setup.sh
@@ -130,36 +130,18 @@ python "${BUDDY_PY}/buddy/validate_env_contents.py" \
   "${IS_R_REQUIRED}"
 
 ###############################################################################
-# If the current project uses R within jupyter:
-# - Ensure an R kernel can be accessed by `jupyter nbconvert` and within
-# `jupyter` by adding `R_KERNEL` to the `kernelspec` list
-#
+# - If the user plans to use R within jupyter, ensure an R kernel is available
+
+JUPYTER_KERNEL_SCRIPT="${SETUP_HELPERS_DIR}/setup_jupyter_r_kernel.sh"
+if [[ ! -f "${JUPYTER_KERNEL_SCRIPT}" ]];
+then
+  die_and_moan \
+  "${0}: '${JUPYTER_KERNEL_SCRIPT}' should exist"
+fi
+
 if [[ ${IS_JUPYTER_R_REQUIRED} -ne 0 ]];
 then
-  if [[ -z "${R_KERNEL}" ]];
-  then
-    die_and_moan \
-    "${0}: 'R_KERNEL' name should be defined in '${JOB_VARS_FILE}'"
-  fi
-  # check if the current kernel name is present in the list of available
-  #  r-kernels for jupyter:
-  # The final ` || : ` step prevents an empty grep-return-value from killing
-  # the script; we do this since CHECK_KERNELS is subsequently double checked
-  # for exact matching against the name of R_KERNEL
-  CHECK_KERNELS=$( jupyter kernelspec list |\
-                   cut -d" " -f3 |\
-                   grep -e "${R_KERNEL}" - ) || :
-  if [[ "${CHECK_KERNELS}" == "${R_KERNEL}" ]];
-  then
-    echo "${0}: Kernel '${R_KERNEL}' is already available" >&2
-  else
-    echo "${0}: Adding Kernel '${R_KERNEL}'" >&2
-    Rscript \
-      -e "kern_name = commandArgs(trailingOnly = TRUE)[1];" \
-      -e "library(IRkernel);" \
-      -e "IRkernel::installspec(name = kern_name, displayname = kern_name);" \
-       "${R_KERNEL}"
-  fi
+  bash "${JUPYTER_KERNEL_SCRIPT}"
 fi
 
 ###############################################################################
@@ -177,7 +159,7 @@ then
   "${0}: '${DIRS_SCRIPT}' should exist"
 fi
 
-bash ${DIRS_SCRIPT}
+bash "${DIRS_SCRIPT}"
 
 ###############################################################################
 # - Construct the R package for this job
@@ -189,7 +171,7 @@ then
   "${0}: '${PKGS_SCRIPT}' should exist"
 fi
 
-bash ${PKGS_SCRIPT}
+bash "${PKGS_SCRIPT}"
 
 ###############################################################################
 # - Setup all subjobs for this job
