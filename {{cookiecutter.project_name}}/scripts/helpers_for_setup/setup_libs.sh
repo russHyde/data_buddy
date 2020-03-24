9c0825ddebfd738d3d450df09d0fda27ddb91c09
Russ Hyde <russHyde@users.noreply.github.com>
2020-03-24 15:01:46 +0000

    Build then install each package (#91)
    Rather than building all packages and then installing them all (which causes dependency problems), we now build/install each package separately.
    
    The remote packages are installed before the local packages.
    
    Removed the (`local_rfuncs`, `global_rfuncs`) directories that used to get copied into a local R package; if the user wants a project-specific package, we now make a package skeleton that the user can modify using standard devtools/usethis/roxygen workflows.
    
    Remote packages are now stored in `./lib/remote` and are not under version control
    Local packages are now stored in `./lib/local` (rather than `./lib/copied_packages` or constructed at setup) and are under version control.
    
    * add some comments
    
    * extract r-builder-script and conda-r-lib into global vars
    
    * add a function to build a project-specific package skeleton
    
    * remove function for setting up r-package structure based on local_rfuncs/global_rfuncs (user should use package-dev workflow)
    
    * add function: buidl and install each package in a directory
    
    * build & install all packages in lib/remote then lib/local
    
    * update comments
    
    * use lib/[built|local|remote] to store R package source/builds
    
    * add details re difference between lib/local and lib/remote
    
    * update example: use ./lib/remote not ./lib/cloned_packages
    
    * R_INCLUDES_FILE and conf/include_into_rpackage are no longer of value, since a straight R package is now used
    
    * update example: lib/conf/include_into_rpackage is not a good example, since that file is no longer used
    
    * Makefile and setup for local R package are removed since we now set up a skeleton that the user updates

--- {{cookiecutter.project_name}}/scripts/helpers_for_setup/setup_libs.sh
+++ {{cookiecutter.project_name}}/scripts/helpers_for_setup/setup_libs.sh
@@ -7,26 +7,9 @@ set -o pipefail
 ###############################################################################
 # Script to setup R package for use in the current project
 #
-# Assumes that all RH-generated R function scripts have been put in
-#   subdirectories of ${LIB_DIR}/global_rfuncs or ${LIB_DIR}/local_rfuncs
-#   and that if the R/ subdirectory of both of these dirs is empty, then
-#   this script aborts, since there is no R package to build if there is no
-#   source code for that package
-#
 # This script should be called by ./scripts/setup.sh
 #   - the global variable IS_R_REQUIRED should exist and should be 1 for this
 #     script to build an R package
-#   - TODO - ? considerations for building python package structures
-#
-# Checks that ${LIB_DIR}/Makefile and ${LIB_DIR}/setup.DESCRIPTION.R exist and
-#   assumes they take arguments 'jobname' and 'r_includes'
-#
-# If the file does not already exist, this script will make the empty file
-#   ${LIB_DIR}/conf/include_these_rpackages.txt
-#   but if it already exists then the file is used as a list of imported
-#   packages
-#   - if this file is empty, it is assumed that the job-specific package does
-#     not need to import any packages
 #
 ###############################################################################
 
@@ -38,39 +21,43 @@ die_and_moan()
 
 ###############################################################################
 
-# define function for setting up the R package:
-function build_r_package {
-  JNAME="${1}"
-  PNAME="${2}"
-  R_INC="${3}"
-  LIB_DIR="${4}"
-  # Check that r_includes is an absolute path to an existing file
-  if [[ "${R_INC:0:1}" != "/" ]] && \
-     [[ "${R_INC:0:2}" != "~" ]] && \
-     [[ "${R_INC:0:2}" != "~/" ]];
+# Function to define a skeleton for a local R package
+
+function define_package_skeleton {
+
+  PKGNAME="${1}"
+  PARENT_DIR="${2}"
+
+  if [[ -z "${PKGNAME}" ]];
   then
     die_and_moan \
-    "${0}: Path to R_INCLUDES should be absolute in setup_libs.sh"
+    "${0}: PKGNAME should be defined when defining the package-skeleton for \
+    \n ... a subjob"
   fi
 
-  # Check that ${LIB_DIR}/Makefile exists
-  # - Any other requirements of ${LIB_DIR}/Makefile should be checked by itself
-  if [[ ! -f "${LIB_DIR}/Makefile" ]];
+  if [[ ! -d "${PARENT_DIR}" ]];
   then
     die_and_moan \
-    "${0}: Makefile for building the libraries/packages for this job is \
-    \n ... missing from lib-dir: ${LIB_DIR}"
+      "${0}: PARENT_DIR should be a directory in 'define_package_skeleton'. \
+      \n ... PARENT_DIR='${PARENT_DIR}'"
   fi
 
-  cd "${LIB_DIR}"
-  make pkgname="${PNAME}" \
-       r_includes="${R_INC}"
-  cd ..
+  # If no local package exists by this name (<PKGNAME>), then initialise one
+  # It's source code will be stored in ./lib/local/<PKGNAME>
+
+  if [[ ! -d "${PARENT_DIR}/${PKGNAME}"  ]];
+  then
+    Rscript \
+      -e "args <- commandArgs(trailingOnly = TRUE);" \
+      -e ".id <- function(x) {x};" \
+      -e "utils::package.skeleton(name = args[1], path = args[2], list = '.id')" \
+      "${PKGNAME}" "${PARENT_DIR}"
+  fi
 }
 
 ###############################################################################
 
-# define function for installing the R package
+# Function for installing an R package
 
 function install_r_package {
   # To call this function:
@@ -104,14 +91,15 @@ function install_r_package {
     ... Current value is ${R_LIB_DIR}"
   fi
 
-  # If PKG_R_DIR does not exist, the package has not been installed yet
+  # If the package does not have a subdirectory in ${R_LIB_DIR}, then it has
+  # not been installed yet
   #  ==> therefore install it
 
-  # Get timestamp for PKG_R_DIR (the version installed in conda-R) and for
+  # Get timestamp for the pkg version installed in conda-R and for
   #   PKG_LOCAL_TAR (the recently built version of the package).
-  # If PKG_LOCAL_TAR is more recent that PKG_R_DIR, then the installed version
-  #   of the package predates the available version
-  # ==> therefore install it
+  # If PKG_LOCAL_TAR is more recent than the installed version, then the
+  # package has been updated
+  #  ==> therefore reinstall it
 
   # By using `R CMD INSTALL` instead of `Rscript -e 'install.packages(...)'` we
   # ensure that if a package fails to install (eg, due to missing dependencies)
@@ -128,6 +116,68 @@ function install_r_package {
 
 ###############################################################################
 
+# For each package in a source directory (typically lib/local or lib/remote),
+# this function will build a tar.gz file from that R package and then install
+# the package from that tar file into the current conda env.
+
+function build_and_install_each_package {
+  # SRC_DIR
+  # BUILD_DIR
+  # CONDA_LIB
+  # BUILDER_SCRIPT
+
+  SRC_DIR="${1}"
+  BUILD_DIR="${2}"
+  CONDA_LIB="${3}"
+  BUILDER_SCRIPT="${4}"
+
+  if [[ ! -d "${BUILD_DIR}" ]]; then mkdir -p "${BUILD_DIR}"; fi
+
+  if [[ ! -f "${BUILDER_SCRIPT}" ]]; then
+    die_and_moan \
+      "${0}: the R-package building script ${BUILDER_SCRIPT} is missing"
+  fi
+
+  for PKG_PATH in \
+    $(find "${SRC_DIR}" -maxdepth 1 -mindepth 1 -type d);
+  do
+    PKG_NAME=$(basename "${PKG_PATH}")
+
+    # Build a tar.gz archive for the current package
+    Rscript "${BUILDER_SCRIPT}" "${PKG_PATH}" "${BUILD_DIR}"
+
+    # Then install the latest .tar.gz corresponding to the current package
+    # (Note that this is a bit of a hack, and may lead to multiple .tar.gz
+    # getting installed sequentially for the same package)
+    for PKG_TAR in \
+      $(find "${LIB_DIR}/built" -name "${PKG_NAME}_*.tar.gz")
+    do
+      # Install the R package if it is newer than the installed R package
+      install_r_package "${PKG_NAME}" "${PKG_TAR}" "${CONDA_LIB}"
+    done
+  done
+}
+
+###############################################################################
+
+# -- script -- #
+
+# This script builds and installs any R packages that are stored in
+# `${LIB_DIR}/remote` or `${LIB_DIR}/local`. "*.tar.gz" files for each package
+# are stored in `${LIB_DIR}/built` and then installed into the CONDA
+# environment.
+#
+# Packages in `./lib/remote` are built/installed first and building/installing
+# occurs sequentially for each package (rather than building all packages and
+# then installing them all)
+#
+# Where the user states to build a project-specific package,
+# (with IS_R_PKG_REQUIRED=1), but such a package does not yet exist, this
+# script initialises an empty package in `${LIB_DIR}/local/<PKGNAME>`. The user
+# should modify this package skeleton as they would for any other R package.
+
+###############################################################################
+
 # This script should have been called from ./scripts/setup.sh
 # Therefore, Check that JOBNAME, PKGNAME and IS_R_REQUIRED are all defined
 # Check that the conda environment for the current job is activated
@@ -136,122 +186,43 @@ function install_r_package {
 if [[ -z "${JOBNAME}" ]] || \
    [[ -z "${IS_R_REQUIRED}" ]] || \
    [[ -z "${IS_R_PKG_REQUIRED}" ]] || \
-   [[ -z "${CONDA_PREFIX}" ]];
+   [[ -z "${CONDA_PREFIX}" ]] || \
+   [[ -z "${LIB_DIR}" ]];
 then
   die_and_moan \
-  "${0}: JOBNAME, IS_R_REQUIRED, IS_R_PKG_REQUIRED and CONDA_PREFIX should be \
-  \n ... defined, CONDA_PREFIX is usually set up by the anaconda environment \
-  \n ... `setup_libs.R` should have been called from `setup.sh`."
+  "${0}: JOBNAME, LIB_DIR, IS_R_REQUIRED, IS_R_PKG_REQUIRED and CONDA_PREFIX \
+  \n ... should be defined. CONDA_PREFIX is usually set up by the anaconda \
+  \n ...  environment. `setup_libs.R` should have been called from `setup.sh`."
 fi
+
+# This script is used to build R packages from source
+R_BUILDER_SCRIPT="${SETUP_HELPERS_DIR}/package_builder.R"
+
+# The R library directory for the current conda environment is:
+CONDA_R_LIB="${CONDA_PREFIX}/lib/R/library"
 
 ###############################################################################
 
-# Call the function for setting up the R package if:
-#   - IS_R_REQUIRED;
-#   - IS_R_PKG_REQUIRED
-#   - and there are some .R scripts in ${LIB_DIR}/local_rfuncs/R or
-#   ${LIB_DIR}/global_rfuncs/R;
-#   - and a Makefile is found in ${LIB_DIR} (checked in build_r_package)
-#   - and a "setup.DESCRIPTION.R" file is found in ${LIB_DIR} (checked in
-#   Makefile)
-#   - and the global vars JOBNAME and PKGNAME are defined (checked above)
-
-# Call the function for installing the R package if additionally:
-#   - the built R-package has never been installed
-#   - or the tar.gz for the built R-package is newer than the installed version
+# If the user wants to make a project-specific package (and one doesn't
+# currently exist), put a skeleton package for holding the source code into
+# ./lib/local/<PKGNAME>
+#
+# The user should modify this skeleton if they want to add extra code and then
+# rerun `./sidekick setup` to build / install it.
 
 if [[ ${IS_R_REQUIRED} -ne 0 ]] && [[ ${IS_R_PKG_REQUIRED} -ne 0 ]];
 then
-  if [[ -z "${PKGNAME}" ]];
-  then
-    die_and_moan \
-    "${0}: PKGNAME should be defined, since IS_R_PKG_REQUIRED is true for this \
-    \n ... project"
-  fi
-
-  # Set default val for the file that specifies which packages to include in
-  # the job-specific pacakge
-  if [[ -z "${R_INCLUDES_FILE}" ]];
-  then
-    R_INCLUDES_FILE="${LIB_DIR}/conf/include_into_rpackage.txt"
-    export R_INCLUDES_FILE
-  fi
-
-  # Determine if there are any function scripts in lib/global_rfuncs/R/*.R or
-  #   lib/local_rfuncs/R/*.R for packaging up
-  #   - The global files should have been copied in by setup_dirs.sh based on
-  #     ./.sidekick/setup/copy_these_files.txt (or cloned from bitbucket)
-  R_FUNCTION_FILES=(`find "${LIB_DIR}/"*_rfuncs/R/ -type f -name "*.R"`)
-
-  {% raw -%}
-  NUM_R_FILES=${#R_FUNCTION_FILES[@]}
-  {%- endraw %}
-
-  # Build/install the package if there are any files to package up and the
-  # scripts required for packaging exist
-  #   -
-  # All built packages should be placed into ${LIB_DIR}/built_packages/
-  # and are installed from this directory
-  if [[ ${NUM_R_FILES} > 0 ]];
-  then
-    build_r_package \
-      "${JOBNAME}" \
-      "${PKGNAME}" \
-      "${R_INCLUDES_FILE}" \
-      "${LIB_DIR}"
-  fi
+  define_package_skeleton "${PKGNAME}" "${LIB_DIR}/local"
 fi
 
-# Where I've copied one of my own packages into the source code for a package,
-#   build that package and put the package-archive into
-#   ${LIB_DIR}/built_packages
-#
-if [[ -d "${LIB_DIR}/cloned_packages" ]] || \
-   [[ -d "${LIB_DIR}/copied_packages" ]];
-then
-  for PKG_PATH in \
-    $(find "${LIB_DIR}/cloned_packages" -maxdepth 1 -mindepth 1 -type d) \
-    $(find "${LIB_DIR}/copied_packages" -maxdepth 1 -mindepth 1 -type d);
-  do
+# Build & Install packages (remotes first)
 
-    R_BUILDER_SCRIPT="${SETUP_HELPERS_DIR}/package_builder.R"
-    if [[ ! -f "${R_BUILDER_SCRIPT}" ]]; then
-      die_and_moan \
-        "${0}: the R-package building script ${R_BUILDER_SCRIPT} is missing"
-    fi
+build_and_install_each_package \
+  "${LIB_DIR}/remote" \
+  "${LIB_DIR}/built" "${CONDA_R_LIB}" ${R_BUILDER_SCRIPT}
 
-    mkdir -p "${LIB_DIR}/built_packages"
-
-    Rscript \
-      "${R_BUILDER_SCRIPT}" \
-      "${PKG_PATH}" \
-      "${LIB_DIR}/built_packages"
-  done
-fi
-
-###############################################################################
-
-# Install any of the packages that were copied into the current R environment
-#
-if [[ -d "${LIB_DIR}/built_packages" ]];
-then
-  for PKG_TAR in \
-    $(find "${LIB_DIR}/built_packages" -name "*.tar.gz");
-  do
-    # The R library directory for the current conda environment is:
-    R_LIB_DIR="${CONDA_PREFIX}/lib/R/library"
-
-    # The package archives are like
-    #   ${LIB_DIR}/built_packages/pkgname_0.1.2.333.tar.gz
-    PKG_NAME=$(basename ${PKG_TAR} | sed -e "s/_*[0-9.]\+tar\.gz//")
-
-    # Install the R package if it is newer than the installed R package
-    install_r_package "${PKG_NAME}" "${PKG_TAR}" "${R_LIB_DIR}"
-  done
-fi
-
-###############################################################################
-# ? skeleton code for building python package?
-# <TODO>
+build_and_install_each_package \
+  "${LIB_DIR}/local" \
+  "${LIB_DIR}/built" "${CONDA_R_LIB}" ${R_BUILDER_SCRIPT}
 
 ###############################################################################
